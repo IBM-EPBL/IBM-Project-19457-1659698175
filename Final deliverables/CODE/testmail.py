945ef643a1b3e1e63483de204d3d097d537f0ebd
PaavaiManimaran <99942998+PaavaiManimaran@users.noreply.github.com>
2022-11-20 14:14:47 +0530

    Create testmail.py

---
+++ Final deliverables/CODE/testmail.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def SendEmail(email, username):
+    FROM_EMAIL =  "plasmadonarapp@gmail.com"
+    TEMPLATE_ID = os.getenv("SENDGRID_TEMPLATE_ID")
+    key = os.getenv("SENDGRID_KEY")
+
+    message = Mail(from_email = FROM_EMAIL,
+    to_emails= email)
+
+    message.dynamic_template_data = {
+        'name': username
+    }
+    
+    message.template_id = TEMPLATE_ID
+
+    try:
+        sg = SendGridAPIClient(key)
+        response = sg.send(message)
+        code, body, headers = response.status_code, response.body, response.headers
+        print(f"Response code: {code}")
+        print(f"Response headers: {headers}")
+        print(f"Response body: {body}")
+        print("Dynamic Messages Sent!")
+    except Exception as e:
+        print(e)
